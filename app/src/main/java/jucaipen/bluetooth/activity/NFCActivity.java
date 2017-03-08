dabc94f406a902bccbca9e66ffbb250eaea56319
langfei yang <185601452@qq.com>
2017-03-08 14:23:35 +0800

    蓝牙通讯  client & server

---
+++ app/src/main/java/jucaipen/bluetooth/activity/NFCActivity.java
@@ -0,0 +1,20 @@
+package jucaipen.bluetooth.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import jucaipen.bluetooth.R;
+
+/**
+ * Created by Administrator on 2017/3/8.
+ */
+
+public class NFCActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc);
+    }
+}
